c23ab44527ed680e83f4c4a438e629fa0d5207ad
morejs <morejs@morejsdeMacBook-Air.local>
2016-09-18 23:29:37 +0800

    feat:add intent to switch activity

---
+++ IntentAndBroadCastReceiver/app/src/main/java/com/example/morejs/intentandbroadcastreceiver/TwoActivity.java
@@ -0,0 +1,16 @@
+package com.example.morejs.intentandbroadcastreceiver;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by morejs on 16/9/18.
+ */
+public class TwoActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two_activity);
+
+    }
+}
